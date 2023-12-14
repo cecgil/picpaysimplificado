bf00cd65071d05e82c7d2f7bf15f701645f6ce3e
cecgil <dudigil6@gmail.com>
2023-12-14 20:10:45 -0300

    feat: initial commit

---
+++ src/main/java/com/picpay/picapaysimplificado/PicapaysimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.picapaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicapaysimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicapaysimplificadoApplication.class, args);
+	}
+
+}
